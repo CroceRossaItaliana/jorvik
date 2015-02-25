4ec0ed297118cc4f728a607610e655c833a1f802
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2015-02-25 22:27:24 +0000

    Persona, Documento, Comitato

--- anagrafica/modelli.py
+++ anagrafica/modelli.py
@@ -11,7 +11,9 @@ Questo modulo definisce i modelli del modulo anagrafico di Gaia.
 """
 
 from base.modelli import *
+from base.stringhe import normalizza_nome, generatore_nome_file
 from base.tratti import *
+from base.stringa import *
 import phonenumbers
 
 
@@ -20,8 +22,97 @@ class Persona(ModelloCancellabile, ConGeolocalizzazioneRaggio):
     Rappresenta un record anagrafico in Gaia.
     """
 
+    # Genere
+    MASCHIO = 'M'
+    FEMMINA = 'F'
+    GENERE = (
+        (MASCHIO, 'Maschio'),
+        (FEMMINA, 'Femmina')
+    )
+
+    # Stati
+    PERSONA = 'P'
+    ASPIRANTE = 'A'
+    VOLONTARIO = 'V'
+    DIPENDENTE = 'D'
+    MILITARE = 'M'
+    STATO = (
+        (PERSONA,       'Persona'),
+        (ASPIRANTE,     'Aspirante'),
+        (VOLONTARIO,    'Volontario'),
+        (DIPENDENTE,    'Dipendente'),
+        (MILITARE,      'Militare'),
+    )
+
+    # Informazioni anagrafiche
+    nome = models.CharField("Nome", max_length=64)
+    cognome = models.CharField("Cognome", max_length=64)
+    codice_fiscale = models.CharField("Codice Fiscale", max_length=16, blank=False, unique=True, db_index=True)
+    data_nascita = models.DateField("Data di nascita", db_index=True)
+    genere = models.CharField("Genere", max_length=1, choices=GENERE, db_index=True)
+
+    # Stato
+    stato = models.CharField("Stato", max_length=1, choices=STATO, db_index=True)
+
+    # Informazioni anagrafiche aggiuntive - OPZIONALI (blank=True o default=..)
+    comune_nascita = models.CharField("Comune di Nascita", max_length=64, blank=True)
+    provincia_nascita = models.CharField("Provincia di Nascita", max_length=2, blank=True)
+    stato_nascita = models.CharField("Stato di nascita", max_length=2, default="IT")
+    indirizzo_residenza = models.CharField("Indirizzo di residenza", max_length=64, blank=True)
+    comune_residenza = models.CharField("Comune di residenza", max_length=64, blank=True)
+    provincia_residenza = models.CharField("Provincia di residenza", max_length=2, blank=True)
+    stato_residenza = models.CharField("Stato di residenza", max_length=2, default="IT")
+    cap_residenza = models.CharField("CAP di Residenza", max_lenght=5, blank=True)
+    email_contatto = models.CharField("Email di contatto", max_length=64, blank=True)
+
+    def nome_completo(self):
+        """
+        Restituisce il nome e cognome
+        :return: "Nome Cognome".
+        """
+        return normalizza_nome(self.nome + " " + self.cognome)
+
+    # Q: Qual e' l'email di questa persona?
+    # A: Una persona puo' avere da zero a due indirizzi email.
+    #    - Persona.email_contatto e' quella scelta dalla persona per il contatto.
+    #    - Persona.utenza.email (o Persona.email_utenza) e' quella utilizzata per accedere al sistema.
+    #    - Persona.email puo' essere usato per ottenere, in ordine, l'email di contatto,
+    #       oppure, se non impostata, quella di utenza. Se nemmeno una utenza e' disponibile,
+    #       viene ritornato None.
+
+    @property
+    def email(self):
+        """
+        Restituisce l'email preferita dalla persona.
+        Se impostata, restituisce l'email di contatto, altrimento l'email di utenza.
+        :return:
+        """
+        if self.email_contatto:
+            return self.email_contatto
+        self.email_utenza
+
+    @property
+    def email_utenza(self):
+        """
+        Restituisce l'email dell'utenza, se presente.
+        :return: Email o None
+        """
+        if self.utenza:
+            return self.utenza.email
+        return None
+
+    def __str__(self):
+        return self.nome_completo()
+
     class Meta:
         verbose_name_plural = "Persone"
+
+    # Q: Qual e' il numero di telefono di questa persona?
+    # A: Una persona puo' avere da zero ad illimitati numeri di telefono.
+    #    - Persona.numeri_telefono ottiene l'elenco di oggetti Telefono.
+    #    - Per avere un elenco di numeri di telefono formattati, usare ad esempio
+    #       numeri = [str(x) for x in Persona.numeri_telefono]
+    #    - Usare Persona.aggiungi_numero_telefono per aggiungere un numero di telefono.
 
     def aggiungi_numero_telefono(self, numero, servizio=False, paese="IT"):
         """
@@ -91,9 +182,25 @@ class Telefono(ModelloSemplice, ConMarcaTemporale):
         return phonenumbers.format_number(self._phonenumber(), phonenumbers.PhoneNumberFormat.E164)
 
 
-
-
 class Documento(ModelloSemplice, ConMarcaTemporale):
+    """
+    Rappresenta un documento caricato da un utente.
+    """
+
+    # Tipologie di documento caricabili
+    CARTA_IDENTITA = 'I'
+    PATENTE_CIVILE = 'P'
+    PATENTE_CRI = 'S'
+    CODICE_FISCALE = 'C'
+    TIPO = (
+        (CARTA_IDENTITA, 'Carta d\'identità'),
+        (PATENTE_CIVILE, 'Patente Civile'),
+        (PATENTE_CRI, 'Patente CRI'),
+        (CODICE_FISCALE, 'Codice Fiscale')
+    )
+
+    persona = models.ForeignKey(Persona, related_name="documenti", db_index=True)
+    file = models.FileField("File", upload_to=generatore_nome_file('documenti/'))
 
     class Meta:
         verbose_name_plural = "Documenti"
@@ -112,6 +219,65 @@ class Appartenenza(ModelloSemplice, ConMarcaTemporale, ConAutorizzazioni):
 
 
 class Comitato(ModelloAlbero, ConGeolocalizzazione):
+
+    # Nome gia' presente in Modello Albero
+
+    # Estensione del Comitato
+    TERRITORIALE = 'T'
+    LOCALE = 'L'
+    PROVINCIALE = 'P'
+    REGIONALE = 'R'
+    NAZIONALE = 'N'
+    ESTENSIONE = (
+        (TERRITORIALE, 'Unità Territoriale'),
+        (LOCALE, 'Comitato Locale'),
+        (PROVINCIALE, 'Comitato Provinciale'),
+        (REGIONALE, 'Comitato Regionale'),
+        (NAZIONALE, 'Comitato Nazioanle')
+    )
+
+    # Tipologia del comitato
+    COMITATO = 'C'
+    MILITARE = 'M'
+    TIPO = (
+        (COMITATO, 'Comitato'),
+        (MILITARE, 'Sede Militare'),
+    )
+
+    estensione = models.CharField("Estensione", max_length=1, choices=ESTENSIONE, db_index=True)
+    tipo = models.CharField("Tipologia", max_length=1, choices=TIPO, default=COMITATO, db_index=True)
+
+    # Dati del comitato
+    # Nota: indirizzo e' gia' dentro per via di ConGeolocalizzazione
+    via = models.CharField("Via", max_length=64, blank=True)
+    civico = models.CharField("Civico", max_length=8, blank=True)
+    comune = models.CharField("Comune", max_length=64, blank=True)
+    provincia = models.CharField("Provincia", max_length=64, blank=True)
+    cap = models.CharField("CAP", max_length=5, blank=True)
+    telefono = models.CharField("Telefono", max_length=64, blank=True)
+    fax = models.CharField("FAX", max_length=64, blank=True)
+    email = models.CharField("Indirizzo e-mail", max_length=64, blank=True)
+    codice_fiscale = models.CharField("Codice Fiscale", max_length=32, blank=True)
+    partita_iva = models.CharField("Partita IVA", max_length=32, blank=True)
+
+    # Q: Come ottengo i dati del comitato?
+    # A: Usa la funzione Comitato.ottieni, ad esempio, Comitato.ottieni('partita_iva').
+    #    Questa funzione cerca la prima partita IVA disponibile a "Salire" l'albero.
+
+    def ottieni(self, nome_campo):
+        """
+        Questa funzione dovrebbe essere usata per ottenere i campi di un comitato.
+        Se il campo non e' definito, risale l'albero per ottenerlo.
+        :param nome_campo: Nome del campo, es. "codice_fiscale", "partita_iva", ecc.
+        :return: Il primo valore disponibile o None se non trovato.
+        """
+        attuale = self
+        while True:
+            if attuale.__getattribute__(nome_campo):
+                return attuale.__getattribute__(nome_campo)
+            if attuale.genitore is None:
+                return None
+            attuale = attuale.genitore
 
     class Meta:
         verbose_name_plural = "Comitati"
---
+++ base/stringhe.py
@@ -0,0 +1,43 @@
+"""
+Funzioni per il trattamento delle stringhe.
+"""
+
+import string
+import uuid
+import os.path
+
+
+def normalizza_nome(stringa):
+    """
+    Normalizza una stringa per l'uso come Nome o titolo.
+    :param stringa: La stringa da normalizzare.
+    :return: La stringa pronta come titolo.
+    """
+    return string.capwords(stringa)
+
+
+def genera_uuid_casuale():
+    """
+    Genera una stringa univoca casuale usando la libreria uuid di python.
+    :return: Qualcosa come 'A943C888-5F0B-40B9-BA80-50AB09C6D1D5'
+    """
+    return str(uuid.uuid4()).upper()
+
+
+def generatore_nome_file(prefisso, forza_suffisso=None):
+    """
+    Ritorna un generatore di nome file casuale, sulla base di un prefisso.
+    :param prefisso: Prefisso (es. "cartella/%Y/")
+    :param forza_suffisso: Un suffisso da forzare (es. ".txt"), o suffisso originale se None
+    :return: Una funzione che ogni volta chiamata ritorna qualcosa come
+             prefisso/C22AF346-8D6B-429B-B518-F85F7E69281F.suffisso
+    """
+
+    def generatore(instanza, originale):
+
+        suffisso = forza_suffisso if forza_suffisso is not None \
+            else os.path.splitext(originale)[1]
+
+        return prefisso + genera_uuid_casuale() + suffisso
+
+    return generatore  # Nota: ritorna una funzione!
