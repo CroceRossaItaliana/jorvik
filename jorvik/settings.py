269cd4e0568b653944a7fdc82fbe19595cbf3b58
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2015-02-27 00:51:02 +0000

    Configurazione

--- jorvik/settings.py
+++ jorvik/settings.py
@@ -72,8 +72,10 @@ WSGI_APPLICATION = 'jorvik.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/config/mysql.cnf',
+        },
     }
 }
 
