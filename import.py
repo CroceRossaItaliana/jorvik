25c6713a370874ecb0511710535a48492a7889f3
AlfioEmanueleFresta <alfio.emanuele.f@gmail.com>
2015-08-19 21:12:31 +0100

    Importazione dei Comitati

---
+++ import.py
@@ -0,0 +1,141 @@
+
+import os, sys
+from django.contrib.gis.geos import Point
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jorvik.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+from django.template.backends import django
+from anagrafica.costanti import NAZIONALE, REGIONALE, PROVINCIALE, LOCALE, TERRITORIALE
+from anagrafica.models import Sede
+from base.geo import Locazione
+
+__author__ = 'alfioemanuele'
+
+import MySQLdb
+
+# .conect(host, username, password, database)
+db = MySQLdb.connect('localhost', 'root', '', 'gaia_vecchio')
+
+
+## IMPORTA COMITATI
+COMITATO_INFERIORE = {
+    'nazionali': 'regionali',
+    'regionali': 'provinciali',
+    'provinciali': 'locali',
+    'locali': 'comitati',
+    'comitati': None
+}
+COMITATO_ESTENSIONE = {
+    'nazionali': NAZIONALE,
+    'regionali': REGIONALE,
+    'provinciali': PROVINCIALE,
+    'locali': LOCALE,
+    'comitati': TERRITORIALE
+}
+COMITATO_GENITORE = {
+    'nazionali': None,
+    'regionali': 'nazionale',
+    'provinciali': 'regionale',
+    'locali': 'provinciale',
+    'comitati': 'locale'
+}
+COMITATO_DATI = {
+    'nazionali': 'datiNazionali',
+    'regionali': 'datiRegionali',
+    'provinciali': 'datiProvinciali',
+    'locali': 'datiLocali',
+    'comitati': 'datiComitati'
+}
+
+def ottieni_comitato(tipo='nazionali', id=1):
+    cursore = db.cursor()
+    cursore.execute("""
+        SELECT
+            id, nome, X(geo), Y(geo)
+        FROM
+            """ + tipo + """
+        WHERE id = %s
+        """, (id,)
+    )
+    comitato = cursore.fetchone()
+    cursore.execute("""
+        SELECT
+            id, nome, valore
+        FROM
+            """ + COMITATO_DATI[tipo] + """
+        WHERE id = %s
+        """, (id,)
+    )
+    dati = cursore.fetchall()
+    dict = {}
+    for (id, nome, valore) in dati:
+        dict.update({nome: valore})
+    return {
+        'id': comitato[0],
+        'nome': comitato[1],
+        'x': comitato[2],
+        'y': comitato[3],
+        'dati': dict
+    }
+
+def ottieni_figli(tipo='nazionali', id=1):
+    if COMITATO_INFERIORE[tipo] is None:
+        return []
+    cursore = db.cursor()
+    cursore.execute("""
+        SELECT
+            id
+        FROM
+            """ + COMITATO_INFERIORE[tipo] + """
+        WHERE """ + COMITATO_GENITORE[COMITATO_INFERIORE[tipo]] + """ = %s
+        """, (id,)
+    )
+    figli = cursore.fetchall()
+    figli = [(COMITATO_INFERIORE[tipo], x[0]) for x in figli]
+    return figli
+
+def locazione(geo, indirizzo):
+    if not indirizzo:
+        return None
+    try:
+        l = Locazione.objects.get(indirizzo=indirizzo)
+    except:
+        l = Locazione(geo=geo, indirizzo=indirizzo)
+        l.save()
+    return l
+
+def carica_comitato(tipo='nazionali', id=1, ref=None, num=0):
+
+    comitato = ottieni_comitato(tipo, id)
+
+    geo = Point(comitato['y'], comitato['x'])
+    try:
+        loc = locazione(geo, comitato['dati']['formattato'])
+    except:
+        loc = None
+
+    c = Sede(
+        genitore=ref,
+        nome=comitato['nome'],
+        locazione=loc,
+        tipo=Sede.COMITATO,
+        estensione=COMITATO_ESTENSIONE[tipo],
+    )
+    c.save()
+    print(" " + ("-"*num) + " " + c.nome + " " + str(geo.x) + ", " + str(geo.y))
+    if c.locazione is not None:
+        print(" " + ("-"*num) + " " + c.nome + " " + str(c.locazione.geo.x) + ", " + str(c.locazione.geo.y))
+
+    totale = 1
+    for (a, b) in ottieni_figli(tipo,id):
+        totale += carica_comitato(a, b, c, num+1)
+
+    return totale
+
+print("Importazione dei Comitati...")
+Sede.objects.all().delete()
+n  = carica_comitato()
+print("Importati " + str(n) + " comitati.")
