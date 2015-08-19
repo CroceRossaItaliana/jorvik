c59a2a1c854cccb7e57260554393d9053e5b68d0
AlfioEmanueleFresta <alfio.emanuele.f@gmail.com>
2015-08-19 16:33:15 +0100

    Admin, Posta: Filtra per data di tentativo, data creazione, ecc.

--- posta/admin.py
+++ posta/admin.py
@@ -9,13 +9,14 @@ __author__ = 'alfioemanuele'
 class AdminMessaggio(admin.ModelAdmin):
     search_fields = ['oggetto', 'mittente__codice_fiscale', 'mittente__email', 'mittente__utenza__email']
     list_display = ('oggetto', 'mittente', 'creazione', 'ultimo_tentativo', 'terminato', )
+    list_filter = ('creazione', 'terminato', 'ultimo_tentativo',)
 
 
 @admin.register(Destinatario)
 class AdminDestinatario(admin.ModelAdmin):
     search_fields = ['messaggio__creazione', 'messaggio__oggetto', 'persona__codice_fiscale', 'persona__email', 'persona__utenza__email']
     list_display = ('messaggio', 'persona', 'inviato', 'tentativo', 'errore')
-    list_filter = ('inviato', )
+    list_filter = ('inviato', 'tentativo', )
 
 
 
