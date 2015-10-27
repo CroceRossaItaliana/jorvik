f4e7ad2358bb41bfaf2250461baf57b57055ce96
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2015-10-27 15:09:23 +0000

    Vagrant

--- config/provision.sh
+++ config/provision.sh
@@ -5,3 +5,24 @@
 # Questo file viene eseguito da Vagrant per fare il provisioning dell'ambiente.
 #
 
+JORVIK_DIRECTORY=/vagrant
+cd $JORVIK_DIRECTORY
+
+sudo bash config/provision-psql.sh
+sudo apt-get upgrade
+sudo apt-get install -y git python3-pip binutils libproj-dev gdal-bin python3-dev libmysqlclient-dev libpq-dev libxml2-dev libxslt-dev
+
+echo "Installazione dei requisiti Python..."
+sudo pip3 install -q -r requirements.txt
+
+echo "Configurazione installazione..."
+cat <<EOL > config/pgsql.cnf
+[client]
+host = localhost
+database = jorvik
+user = jorvik
+password = jorvik
+EOL
+
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
