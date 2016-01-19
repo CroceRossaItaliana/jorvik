673a06cc54c46b19d2d095dc7d4f0e7c903e8a95
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-01-19 10:59:25 +0000

    Permetti elenchi soci passati

--- ufficio_soci/forms.py
+++ ufficio_soci/forms.py
@@ -28,12 +28,6 @@ class ModuloElencoSoci(forms.Form):
     al_giorno = forms.DateField(help_text="La data alla quale generare l'elenco soci.",
                                 required=True, initial=datetime.date.today)
 
-    def clean_al_giorno(self):  # Non permette date passate
-        al_giorno = self.cleaned_data['al_giorno']
-        if al_giorno < datetime.date.today():
-            raise forms.ValidationError("La data deve essere futura.")
-        return al_giorno
-
 
 class ModuloElencoElettorato(forms.Form):
     ELETTORATO_ATTIVO = 'A'
