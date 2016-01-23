9c6a6948c35cf7d5e8be85ca615833ecc21cf95e
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-01-23 18:04:00 +0000

    Ripristinato import strict

--- anagrafica/importa.py
+++ anagrafica/importa.py
@@ -2,10 +2,8 @@ import datetime
 import re
 
 import codicefiscale
-import ftfy
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.utils.encoding import smart_str
 
 from anagrafica.models import Persona, Sede, Appartenenza
 from autenticazione.models import Utenza
@@ -17,16 +15,6 @@ VALIDAZIONE_OK = "OK"
 VALIDAZIONE_AVVISO = "AVVISO"
 VALIDAZIONE_ERRORE = "ERRORE"
 
-
-def stringa(s):
-    if s is None:
-        return ''
-    # try:
-    #    #return smart_str(s.encode('utf-8'))
-    #except:
-    return ftfy.fix_text(smart_str(s), fix_entities=False)
-
-
 def import_valida_volontario_riga(riga):
 
     for i in range(0, len(riga)):
@@ -34,11 +22,11 @@ def import_valida_volontario_riga(riga):
 
     log = []
 
-    nome = normalizza_nome(stringa(riga[0]))
+    nome = normalizza_nome(riga[0])
     if not nome:
         log += [(VALIDAZIONE_ERRORE, "Nome vuoto")]
 
-    cognome = normalizza_nome(stringa(riga[1]))
+    cognome = normalizza_nome(riga[1])
     if not cognome:
         log += [(VALIDAZIONE_ERRORE, "Cognome vuoto")]
 
@@ -47,37 +35,32 @@ def import_valida_volontario_riga(riga):
     try:
         data_nascita = datetime.datetime.strptime(riga[2], "%d/%m/%Y").date()
     except ValueError:
-        data_nascita = None
-        #log += [(VALIDAZIONE_ERRORE, "Data di nascita non valida (gg/mm/YYYY)")]
+        log += [(VALIDAZIONE_ERRORE, "Data di nascita non valida (gg/mm/YYYY)")]
 
     if data_nascita and data_nascita < datetime.date(1800, 1, 1):
         log += [(VALIDAZIONE_ERRORE, "Data di nascita errata (prima del 1800)")]
 
-    luogo_nascita = normalizza_nome(stringa(riga[3]))
-    luogo_nascita = re.search("(.*) ?\((.*)\)", luogo_nascita)
-    #if not luogo_nascita:
-    #    log += [(VALIDAZIONE_ERRORE, "Luogo di nascita non valido (Comune (PV))")]
+    luogo_nascita = normalizza_nome(riga[3])
+    luogo_nascita = re.search("(.*) \((.*)\)", luogo_nascita)
+    if not luogo_nascita:
+        log += [(VALIDAZIONE_ERRORE, "Luogo di nascita non valido (Comune (PV))")]
 
     comune_nascita = luogo_nascita.group(1) if luogo_nascita else None
     provincia_nascita = luogo_nascita.group(2) if luogo_nascita else None
 
     if provincia_nascita and len(provincia_nascita) > 2:
-        provincia_nascita = provincia_nascita[:2]
-        #log += [(VALIDAZIONE_ERRORE, "Provincia di nascita non valida (>2 caratteri)")]
+        log += [(VALIDAZIONE_ERRORE, "Provincia di nascita non valida (>2 caratteri)")]
 
     codice_fiscale = riga[4].upper()
-    #if not codicefiscale.isvalid(codice_fiscale):
-    #    log += [(VALIDAZIONE_ERRORE, "Codice fiscale non valido")]
+    if not codicefiscale.isvalid(codice_fiscale):
+        log += [(VALIDAZIONE_ERRORE, "Codice fiscale non valido")]
 
-    sede = stringa(riga[15])
+    sede = riga[15]
     try:
         sede = Sede.objects.get(nome__iexact=sede)
     except Sede.DoesNotExist:
-        try:
-            sede = Sede.objects.get(nome__iexact=stringa(riga[16]))
-        except:
-            log += [(VALIDAZIONE_ERRORE, "Sede non trovata: %s" % (sede,))]
-            sede = None
+        log += [(VALIDAZIONE_ERRORE, "Sede non trovata: %s" % (sede,))]
+        sede = None
 
 
     email = riga[10]
@@ -95,12 +78,12 @@ def import_valida_volontario_riga(riga):
     if sede:
         if precedente:
 
-            presso_mia_sede = precedente.appartenenze_attuali().filter(membro=Appartenenza.VOLONTARIO, sede__in=sede.comitato.espandi(includi_me=True)).first()
-            presso_qualche_sede = precedente.appartenenze_attuali(membro=Appartenenza.VOLONTARIO).exists()
+            presso_mia_sede = precedente.appartenenze_attuali().filter(sede__in=sede.comitato.espandi(includi_me=True)).first()
+            presso_qualche_sede = precedente.appartenenze_attuali().exists()
             aspirante = Aspirante.objects.filter(persona=precedente).exists()
 
             if presso_mia_sede:
-                log += [(VALIDAZIONE_ERRORE, "Già appartenente come %s presso la sede richiesta di %s" % (presso_mia_sede.get_membro_display(), presso_mia_sede.sede.nome_completo))]
+                log += [(VALIDAZIONE_ERRORE, "Già appartenente come %s presso %s" % (presso_mia_sede.get_membro_display(), presso_mia_sede.sede.nome_completo))]
 
             elif presso_qualche_sede:
                 log += [(VALIDAZIONE_ERRORE, "Già appartenente presso Sede fuori da %s" % (sede.comitato.nome,))]
@@ -110,7 +93,7 @@ def import_valida_volontario_riga(riga):
                                              "sua appartenenza e disattivata la possibilita di partecipare a nuovi corsi base. ")]
 
             else:
-                log += [(VALIDAZIONE_AVVISO, "Esiste in Gaia ma senza alcuna appartenenza come volontario")]
+                log += [(VALIDAZIONE_AVVISO, "Esiste in Gaia ma senza alcuna appartenenza")]
 
         else:
 
@@ -133,17 +116,17 @@ def import_valida_volontario_riga(riga):
     if indirizzo_residenza == ", ":
         indirizzo_residenza = ""
 
-    comune_residenza = stringa(riga[7])
-    #if not comune_residenza:
-    #    log += [(VALIDAZIONE_ERRORE, "Comune di residenza obbligatorio")]
+    comune_residenza = riga[7]
+    if not comune_residenza:
+        log += [(VALIDAZIONE_ERRORE, "Comune di residenza obbligatorio")]
 
-    provincia_residenza = stringa(riga[8])
-    #if not provincia_residenza:
-    #    log += [(VALIDAZIONE_ERRORE, "Provincia di residenza obbligatorio")]
+    provincia_residenza = riga[8]
+    if not provincia_residenza:
+        log += [(VALIDAZIONE_ERRORE, "Provincia di residenza obbligatorio")]
 
     cap = riga[9]
-    #if not cap:
-    #    log += [(VALIDAZIONE_ERRORE, "CAP di residenza obbligatorio")]
+    if not cap:
+        log += [(VALIDAZIONE_ERRORE, "CAP di residenza obbligatorio")]
 
     telefono = riga[12]
     telefono_servizio = riga[13]
@@ -261,3 +244,4 @@ def import_import_volontari(risultato):
         i += 1
 
     return i
+
