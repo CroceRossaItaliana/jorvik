d9e22183ea2684494aa5fc399719996da134b849
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-01-30 16:07:24 +0000

    New Relic agent installed

--- jorvik/wsgi.py
+++ jorvik/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jorvik.settings")
 
+# New Relic Agent (env: NEW_RELIC_CONFIG_FILE)
+newrelic.agent.initialize()
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(get_wsgi_application())
