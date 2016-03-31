1b48d3ea183938f94f74ea47594cdc75b54ae65b
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-03-31 17:27:10 +0100

    Fix #201 rinomina i comitati, aggiorna ragione sociale

--- anagrafica/migrations/0038_issue201_ragione_sociale_comitati.py
+++ anagrafica/migrations/0038_issue201_ragione_sociale_comitati.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+
 def forwards_func(apps, schema_editor):
     Sede = apps.get_model("anagrafica", "Sede")
 
@@ -10,15 +11,24 @@ def forwards_func(apps, schema_editor):
     ex_provinciali = Sede.objects.filter(nome__contains="Provinciale")
 
     comitati = ex_locali | ex_provinciali
+    comitati = comitati.order_by('nome')
 
+    # print("")
+    # print("| Ragione sociale originale | Nuova ragione sociale |")
+    # print("|----------|----------|")
     for l in comitati:
         vecchio_nome = l.nome
-        nuovo_nome = vecchio_nome.replace(" Locale", "").replace(" Provinciale", "")
-        print("%s => %s" % (vecchio_nome, nuovo_nome))
-        l.nome = nuovo_nome
-        l.save()
-
-    raise ValueError("Non pronto!")
+        nuovo_nome = vecchio_nome\
+                     .replace("Comitato Provinciale Di Roma", "Comitato dell'Area Metropolitana di Roma Capitale")\
+                     .replace("Comitato Provinciale Di Trento", "Comitato della Provincia Autonoma di Trento")\
+                     .replace("Comitato Provinciale Di Bolzano", "Comitato della Provincia Autonoma di Bolzano")\
+                     .replace(" Locale", "")\
+                     .replace(" Provinciale", "")\
+                     .replace(" Di ", " di ").replace(" Della ", " della ").replace(" Dell'", " dell'").replace(" Delle ", " delle ")
+        if vecchio_nome != nuovo_nome:
+            # print("| %s | %s |" % (vecchio_nome, nuovo_nome))
+            l.nome = nuovo_nome
+            l.save()
 
 
 class Migration(migrations.Migration):
