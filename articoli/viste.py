a97efe3fd1381b035f2e7e0a0520527a05c871d9
Marco Federighi <m.federighi@nephila.it>
2016-04-29 16:52:16 +0200

    Aggiunta base per le viste degli Articoli

--- articoli/viste.py
+++ articoli/viste.py
@@ -1,8 +1,8 @@
+from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
 from articoli.models import Articolo
-from autenticazione.funzioni import pagina_privata, pagina_pubblica
 
 
 class ListaArticoli(ListView):
@@ -11,7 +11,28 @@ class ListaArticoli(ListView):
     template_name = 'lista_articoli.html'
 
     def get_queryset(self):
-        return Articolo.objects.pubblicati()
+        utente = self.request.user
+        if isinstance(utente, AnonymousUser):
+            return None
+        persona = utente.persona
+        articoli = Articolo.objects.pubblicati()
+        id_articoli = []
+        if persona:
+            for articolo in articoli:
+                segmenti = articolo.segmenti.all()
+                if not segmenti:
+                    id_articoli.append(articolo.pk)
+                    break
+                appartiene = False
+                for segmento in segmenti:
+                    if persona.appartiene_al_segmento(segmento):
+                        appartiene = True
+                        break
+                if appartiene:
+                    id_articoli.append(articolo.pk)
+        if id_articoli:
+            articoli = articoli.filter(pk__in=id_articoli)
+        return articoli
 
 
 class DettaglioArticolo(DetailView):
@@ -20,3 +41,22 @@ class DettaglioArticolo(DetailView):
     slug_url_kwarg = 'articolo_pk'
     context_object_name = 'articolo'
     template_name = 'dettaglio_articolo.html'
+
+    def get_object(self):
+        utente = self.request.user
+        if isinstance(utente, AnonymousUser):
+            return None
+        persona = utente.persona
+        obj = super(DetailView, self).get_object()
+        if persona:
+            segmenti = obj.segmenti.all()
+            if not segmenti:
+                obj.incrementa_visualizzazioni()
+                return obj
+            for segmento in segmenti:
+                if persona.appartiene_al_segmento(segmento):
+                    obj.incrementa_visualizzazioni()
+                    return obj
+            return None
+        obj.incrementa_visualizzazioni()
+        return obj
