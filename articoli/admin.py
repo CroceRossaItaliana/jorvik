281ae9153a2b3c0fb6015bfdc1eefe8752ac714b
Marco Federighi <m.federighi@nephila.it>
2016-05-02 12:02:46 +0200

    Aggiungi action per pubblicare o passare allo stato bozza gli articoli

--- articoli/admin.py
+++ articoli/admin.py
@@ -12,6 +12,7 @@ class ArticoloSegmentoInline(admin.TabularInline):
 class AdminArticolo(admin.ModelAdmin):
     inlines = (ArticoloSegmentoInline,)
     readonly_fields = ('visualizzazioni',)
+    actions = ['pubblica', 'bozza']
     fieldsets = (
         (None, {
             'fields': ('titolo', 'slug', 'corpo', 'estratto',)
@@ -20,3 +21,21 @@ class AdminArticolo(admin.ModelAdmin):
             'fields': ('autore', 'data_inizio_pubblicazione', 'data_fine_pubblicazione','stato', 'visualizzazioni'),
         }),
     )
+
+    def pubblica(self, request, queryset):
+        rows_updated = queryset.update(stato=Articolo.PUBBLICATO)
+        if rows_updated == 1:
+            message_bit = "1 articolo è stato"
+        else:
+            message_bit = "%s articoli sono stati" % rows_updated
+        self.message_user(request, "%s pubblicati con successo." % message_bit)
+    pubblica.short_description = "Pubblica uno o più articoli"
+
+    def bozza(self, request, queryset):
+        rows_updated = queryset.update(stato=Articolo.BOZZA)
+        if rows_updated == 1:
+            message_bit = "1 articolo è stato"
+        else:
+            message_bit = "%s articoli sono stati" % rows_updated
+        self.message_user(request, "%s passato allo stato bozza con successo." % message_bit)
+    spubblica.short_description = "Passa allo stato bozza uno o più articoli"
