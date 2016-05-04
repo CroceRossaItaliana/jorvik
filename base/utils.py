0100bdd9a6b43378271c57667e1278df5c2b96d9
Marco Federighi <m.federighi@nephila.it>
2016-05-04 16:46:32 +0200

    Rimosso vecchio codice

--- base/utils.py
+++ base/utils.py
@@ -158,15 +158,6 @@ class UpperCaseCharField(models.CharField):
     def __init__(self, *args, **kwargs):
         super(UpperCaseCharField, self).__init__(*args, **kwargs)
 
-    def pre_save(self, model_instance, add):
-        value = getattr(model_instance, self.attname, None)
-        if value:
-            value = value.upper()
-            setattr(model_instance, self.attname, value)
-            return value
-        else:
-            return super(UpperCaseCharField, self).pre_save(model_instance, add)
-
     def to_python(self, value):
         return value.upper()
 
