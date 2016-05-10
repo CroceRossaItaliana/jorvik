c162f6c7abdb8c5879e41900944eee2f04a4167c
Iacopo Spalletti <i.spalletti@nephila.it>
2016-05-10 16:06:49 +0200

    Ottimizzazione grafica area articoli

--- articoli/admin.py
+++ articoli/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from articoli.models import Articolo, ArticoloSegmento
@@ -9,15 +10,27 @@ class ArticoloSegmentoInline(admin.TabularInline):
     raw_id_fields = ('sede', 'titolo')
 
 
+class ArticoloAdminForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ArticoloAdminForm, self).__init__(*args, **kwargs)
+        self.fields['estratto'].widget = forms.Textarea()
+        self.fields['estratto'].help_text = 'L\'estratto è limitato a 1024 caratteri, il testo eccedente sarà tagliato'
+
+    class Meta:
+        model = Articolo
+        fields = '__all__'
+
+
 @admin.register(Articolo)
 class AdminArticolo(admin.ModelAdmin):
+    form = ArticoloAdminForm
     inlines = (ArticoloSegmentoInline,)
     readonly_fields = ('visualizzazioni',)
     actions = ['pubblica', 'bozza']
-    prepopulated_fields = {'slug': ('titolo',)}
     fieldsets = (
         (None, {
-            'fields': ('titolo', 'slug', 'corpo', 'estratto',)
+            'fields': ('titolo', 'corpo', 'estratto',)
         }),
         ('Informazioni', {
             'fields': (
