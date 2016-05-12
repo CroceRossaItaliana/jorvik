f1d7d31403146f26e307b493a04ca9d038135737
marco federighi <federighi.marco@gmail.com>
2016-05-12 23:17:49 +0200

    Migliorata logica e aggiunto test

--- base/autorizzazioni_test/models.py
+++ base/autorizzazioni_test/models.py
@@ -8,15 +8,18 @@ from base.tratti import ConMarcaTemporale
 
 class BaseAutorizzazioneTest(ModelloSemplice, ConMarcaTemporale, ConAutorizzazioni):
 
-    richiedente = models.ForeignKey(Persona, related_name='autorizzazioni_test_richieste_da', on_delete=models.SET_NULL, null=True)
-    persona = models.ForeignKey(Persona, related_name='autorizzazioni_test', on_delete=models.CASCADE)
-    destinazione = models.ForeignKey(Sede, related_name='autorizzazioni_test_destinazione', on_delete=models.PROTECT)
-    appartenenza = models.ForeignKey(Appartenenza, related_name='autorizzazioni_test', null=True, blank=True, on_delete=models.CASCADE)
+    richiedente = models.ForeignKey(Persona, related_name='%(app_label)s_%(class)s_autorizzazioni_test_richieste_da', on_delete=models.SET_NULL, null=True)
+    persona = models.ForeignKey(Persona, related_name='%(app_label)s_%(class)s_autorizzazioni_test_persona', on_delete=models.CASCADE)
+    destinazione = models.ForeignKey(Sede, related_name='%(app_label)s_%(class)s_autorizzazioni_test_destinazione', on_delete=models.PROTECT)
+    appartenenza = models.ForeignKey(Appartenenza, related_name='%(app_label)s_%(class)s_autorizzazioni_test_appartenenza', null=True, blank=True, on_delete=models.CASCADE)
     protocollo_numero = models.CharField('Numero di protocollo', max_length=512, null=True, blank=True)
     protocollo_data = models.DateField('Data di presa in carico', null=True, blank=True)
     motivo = models.CharField(max_length=4096, null=True, blank=False,)
 
     RICHIESTA_NOME = "AutorizzazioneTest"
+
+    class Meta:
+        abstract = True
 
     def richiedi(self):
         if not self.persona.sede_riferimento():
@@ -40,8 +43,16 @@ class NegaAutorizzazioneTest(BaseAutorizzazioneTest):
             self.confermata = False
             self.save()
         elif auto and not modulo:
+            presidente = self.richiedente.sede_riferimento().presidente()
+            autorizzazione = self.autorizzazioni.first()
+            autorizzazione.concessa = False
+            autorizzazione.firmatario = presidente
+            autorizzazione.necessaria = False
+            autorizzazione.save()
+
             self.confermata = False
             self.save()
+            self.autorizzazioni.update(necessaria=False)
 
 
 class ApprovaAutorizzazioneTest(BaseAutorizzazioneTest):
@@ -53,9 +64,15 @@ class ApprovaAutorizzazioneTest(BaseAutorizzazioneTest):
             self.protocollo_data = modulo.cleaned_data['protocollo_data']
             self.protocollo_numero = modulo.cleaned_data['protocollo_numero']
             self.confermata = True
-            self.ritirata = False
             self.save()
         elif auto and not modulo:
-            self.confermata = True
-            self.ritirata = False
-            self.save()
+            presidente = self.richiedente.sede_riferimento().presidente()
+            autorizzazione = self.autorizzazioni.first()
+            autorizzazione.concessa = True
+            autorizzazione.firmatario = presidente
+            autorizzazione.necessaria = False
+            autorizzazione.save()
+            self.autorizzazioni.filter(progressivo=autorizzazione.progressivo).update(necessaria=False)
+            if self.autorizzazioni.filter(necessaria=True).count() == 0:
+                self.confermata = True
+                self.save()
