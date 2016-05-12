875c7fc01afbf2f4ea5c687ba2fa0822b2b0914f
marco federighi <federighi.marco@gmail.com>
2016-05-12 23:27:01 +0200

    Refactoring minore e aggiunta test

--- base/autorizzazioni_test/test.py
+++ base/autorizzazioni_test/test.py
@@ -49,7 +49,7 @@ class TestAutorizzazioni(TestCase):
             motivo='Un motivo qualsiasi'
         )
 
-        da_approvare.creazione = datetime.datetime.now() - datetime.timedelta(days=10)
+        da_approvare.creazione = datetime.datetime.now() - datetime.timedelta(days=60)
         da_approvare.save()
         da_approvare.richiedi()
 
@@ -74,6 +74,59 @@ class TestAutorizzazioni(TestCase):
         )
 
         self.assertIn(da_approvare, ApprovaAutorizzazioneTest.con_esito_ok())
+
+    def test_autorizzazione_accettata_fail(self):
+        presidente = crea_persona()
+        presidente, sede, _ = crea_persona_sede_appartenenza(presidente)
+        persona = crea_persona()
+        appartenenza = crea_appartenenza(persona, sede)
+
+        delega_presidente = Delega(
+            persona=presidente,
+            tipo=PRESIDENTE,
+            oggetto=sede,
+            inizio=datetime.datetime.now() - datetime.timedelta(days=50),
+            fine=datetime.datetime.now() + datetime.timedelta(days=50)
+        )
+        delega_presidente.save()
+
+        self.assertFalse(
+            presidente.autorizzazioni_in_attesa().exists(),
+            msg="Il presidente non ha autorizzazioni in attesa"
+        )
+
+        da_approvare = ApprovaAutorizzazioneTest.objects.create(
+            richiedente=persona,
+            persona=persona,
+            destinazione=sede,
+            appartenenza=appartenenza,
+            motivo='Un motivo qualsiasi'
+        )
+
+        da_approvare.richiedi()
+
+
+        self.assertTrue(
+            presidente.autorizzazioni_in_attesa().exists(),
+            msg="Il presidente ha autorizzazioni da processare"
+        )
+
+        aut = presidente.autorizzazioni_in_attesa().first()
+
+        self.assertTrue(aut)
+
+        da_approvare.controlla_concedi_automatico()
+
+        aut = presidente.autorizzazioni_in_attesa().first()
+        self.assertTrue(aut)
+
+        self.assertTrue(
+            presidente.autorizzazioni_in_attesa().exists(),
+            msg="Il presidente ha autorizzazioni da processare"
+        )
+
+        self.assertNotIn(da_approvare, ApprovaAutorizzazioneTest.con_esito_ok())
+
 
     def test_autorizzazione_negata(self):
         presidente = crea_persona()
@@ -103,7 +156,7 @@ class TestAutorizzazioni(TestCase):
             motivo='Un motivo qualsiasi'
         )
 
-        da_approvare.creazione = datetime.datetime.now() - datetime.timedelta(days=10)
+        da_approvare.creazione = datetime.datetime.now() - datetime.timedelta(days=60)
         da_approvare.save()
         da_approvare.richiedi()
 
@@ -123,3 +176,50 @@ class TestAutorizzazioni(TestCase):
         self.assertFalse(aut)
 
         self.assertIn(da_approvare, NegaAutorizzazioneTest.con_esito_no())
+
+    def test_autorizzazione_negata_fail(self):
+        presidente = crea_persona()
+        presidente, sede, _ = crea_persona_sede_appartenenza(presidente)
+        persona = crea_persona()
+        appartenenza = crea_appartenenza(persona, sede)
+
+        delega_presidente = Delega(
+            persona=presidente,
+            tipo=PRESIDENTE,
+            oggetto=sede,
+            inizio=datetime.datetime.now() - datetime.timedelta(days=50),
+            fine=datetime.datetime.now() + datetime.timedelta(days=50)
+        )
+        delega_presidente.save()
+
+        self.assertFalse(
+            presidente.autorizzazioni_in_attesa().exists(),
+            msg="Il presidente non ha autorizzazioni in attesa"
+        )
+
+        da_approvare = NegaAutorizzazioneTest.objects.create(
+            richiedente=persona,
+            persona=persona,
+            destinazione=sede,
+            appartenenza=appartenenza,
+            motivo='Un motivo qualsiasi'
+        )
+
+        da_approvare.richiedi()
+
+
+        self.assertTrue(
+            presidente.autorizzazioni_in_attesa().exists(),
+            msg="Il presidente ha autorizzazioni da processare"
+        )
+
+        aut = presidente.autorizzazioni_in_attesa().first()
+
+        self.assertTrue(aut)
+
+        da_approvare.controlla_nega_automatico()
+
+        aut = presidente.autorizzazioni_in_attesa().first()
+        self.assertTrue(aut)
+
+        self.assertNotIn(da_approvare, NegaAutorizzazioneTest.con_esito_no())
