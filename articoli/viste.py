01142042177378c943a80ad6afa6635e744d0d9b
Iacopo Spalletti <i.spalletti@nephila.it>
2016-05-13 17:17:52 +0200

    Ritorna 404 se l'utente non ha i permessi per l'articolo

--- articoli/viste.py
+++ articoli/viste.py
@@ -8,18 +8,13 @@ from articoli.models import Articolo, ArticoloSegmento
 from autenticazione.funzioni import pagina_privata, VistaDecorata
 
 
-class ListaArticoli(VistaDecorata, ListView):
-    model = Articolo
-    context_object_name = 'articoli'
-    template_name = 'lista_articoli.html'
-    paginate_by = 10
+class FiltraSegmenti(object):
 
-    @method_decorator(pagina_privata)
-    def dispatch(self, request, *args, **kwargs):
-        return super(ListaArticoli, self).dispatch(request, *args, **kwargs)
+    @property
+    def persona(self):
+        return self.request.user.persona
 
-    def get_context_data(self, **kwargs):
-        context = super(ListaArticoli, self).get_context_data(**kwargs)
+    def get_queryset(self):
         filtri_extra = {}
         anno = self.kwargs.get('anno', '')
         mese = self.kwargs.get('mese', '')
@@ -38,21 +33,35 @@ class ListaArticoli(VistaDecorata, ListView):
             filtri_extra['data_inizio_pubblicazione__year'] = anno
         if mese:
             filtri_extra['data_inizio_pubblicazione__month'] = mese
-        utente = self.request.user
-        persona = utente.persona
-        articoli_segmenti = ArticoloSegmento.objects.all().filtra_per_segmenti(persona)
-        articoli = articoli_segmenti.oggetti_collegati().pubblicati().filter(**filtri_extra)
-        anni = articoli.extra(select={'anno': 'extract( year from data_inizio_pubblicazione )'}).values_list('anno', flat=True).order_by().annotate(Count('id'))
+        articoli_segmenti = ArticoloSegmento.objects.all().filtra_per_segmenti(self.persona)
+        articoli = articoli_segmenti.oggetti_collegati().pubblicati()
+        return articoli.filter(**filtri_extra)
+
+
+class ListaArticoli(FiltraSegmenti, VistaDecorata, ListView):
+    model = Articolo
+    context_object_name = 'articoli'
+    template_name = 'lista_articoli.html'
+    paginate_by = 10
+
+    @method_decorator(pagina_privata)
+    def dispatch(self, request, *args, **kwargs):
+        return super(ListaArticoli, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ListaArticoli, self).get_context_data(**kwargs)
+        anno = self.kwargs.get('anno', '')
+        mese = self.kwargs.get('mese', '')
+        anni = context['articoli'].extra(select={'anno': 'extract( year from data_inizio_pubblicazione )'}).values_list('anno', flat=True).order_by().annotate(Count('id'))
         anni = sorted(set([int(anno) for anno in anni] + [date.today().year]))
         context['anni'] = anni
         context['mesi'] = [('%0d' % i, date(year=date.today().year, month=i, day=1)) for i in range(1, 12)]
         context['mese_selezionato'] = '%0s' % mese
         context['anno_selezionato'] = anno
-        context['articoli'] = articoli
         return context
 
 
-class DettaglioArticolo(VistaDecorata, DetailView):
+class DettaglioArticolo(FiltraSegmenti, VistaDecorata, DetailView):
     model = Articolo
     slug_field = 'slug'
     slug_url_kwarg = 'articolo_slug'
@@ -63,12 +72,8 @@ class DettaglioArticolo(VistaDecorata, DetailView):
     def dispatch(self, request, *args, **kwargs):
         return super(DettaglioArticolo, self).dispatch(request, *args, **kwargs)
 
-    def get_object(self):
-        utente = self.request.user
-        persona = utente.persona
-        obj = super(DetailView, self).get_object()
-        articoli_segmenti = ArticoloSegmento.objects.all().filtra_per_segmenti(persona)
-        articoli = articoli_segmenti.oggetti_collegati().pubblicati().filter(pk=obj.pk)
-        if articoli.count() == 1:
+    def get_object(self, queryset=None):
+        obj = super(DetailView, self).get_object(queryset)
+        if obj:
             obj.incrementa_visualizzazioni()
-            return obj
+        return obj
