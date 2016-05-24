5c5c4b2b4ddf4d77568854cc0352671437198851
Marco Federighi <m.federighi@nephila.it>
2016-05-24 18:04:18 +0200

    WIP: scheletro gestione richieste tesserini duplicati

--- ufficio_soci/viste.py
+++ ufficio_soci/viste.py
@@ -1067,6 +1067,8 @@ def us_tesserini_richiedi(request, me, persona_pk=None):
     """
     persona = get_object_or_404(Persona, pk=persona_pk)
 
+    tipo_richiesta = Tesserino.RILASCIO
+
     if not me.permessi_almeno(persona, MODIFICA):
         return redirect(ERRORE_PERMESSI)
 
@@ -1091,9 +1093,7 @@ def us_tesserini_richiedi(request, me, persona_pk=None):
                                          "confermata su Gaia.", **torna)
 
     if persona.tesserini.filter(stato_richiesta__in=(Tesserino.RICHIESTO, Tesserino.ACCETTATO)).exists():
-        return errore_generico(request, me, titolo="Tesserino già richiesto",
-                               messaggio="Una richiesta è già stata inoltrata per "
-                                         "il volontario selezionato.", **torna)
+        tipo_richiesta = Tesserino.DUPLICATO
 
     comitato = sede.comitato
     if not comitato.locazione:
@@ -1113,7 +1113,7 @@ def us_tesserini_richiedi(request, me, persona_pk=None):
     tesserino = Tesserino(
         persona=persona,
         emesso_da=regionale,
-        tipo_richiesta=Tesserino.RILASCIO,
+        tipo_richiesta=tipo_richiesta,
         stato_richiesta=Tesserino.RICHIESTO,
         richiesto_da=me
     )
