ecb16a11dfb21424c4616794a1793fd38ed1b24e
Marco Federighi <m.federighi@nephila.it>
2016-06-06 17:56:39 +0200

    Estesa gestione autorizzazioni

--- base/cron.py
+++ base/cron.py
@@ -1,6 +1,6 @@
 from django_cron import CronJobBase, Schedule
 
-from base.models import Allegato
+from base.models import Allegato, Autorizzazione
 
 
 class CronCancellaFileScaduti(CronJobBase):
@@ -14,3 +14,14 @@ class CronCancellaFileScaduti(CronJobBase):
     def do(self):
         n = Allegato.pulisci()
         print("Sono stati rimossi %d file scaduti." % n)
+
+class CronApprovaNegaAuto(CronJobBase):
+
+    RUN_EVERY_HOURS = 24
+    RUN_EVERY_MINS = RUN_EVERY_HOURS * 60
+
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'base.autorizzazioni.automatiche'
+
+    def do(self):
+        n, m = Autorizzazione.gestisci_automatiche()
