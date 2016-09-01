22cf6bb0358101303c59a463d7e4f21ca8b75d61
Paolo Romolini <p.romolini@nephila.it>
2016-09-01 16:08:34 +0200

    Aggiunge il mixin readonly a tutti i model admin

--- formazione/admin.py
+++ formazione/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from base.admin import InlineAutorizzazione
 from formazione.models import CorsoBase, PartecipazioneCorsoBase, AssenzaCorsoBase, Aspirante, LezioneCorsoBase
+from gruppi.readonly_admin import ReadonlyAdminMixin
+
 
 __author__ = 'alfioemanuele'
 
@@ -12,19 +14,19 @@ RAW_ID_FIELDS_ASSENZACORSOBASE = ['lezione', 'persona', 'registrata_da',]
 RAW_ID_FIELDS_ASPIRANTE = ['persona', 'locazione',]
 
 
-class InlinePartecipazioneCorsoBase(admin.TabularInline):
+class InlinePartecipazioneCorsoBase(ReadonlyAdminMixin, admin.TabularInline):
     model = PartecipazioneCorsoBase
     raw_id_fields = RAW_ID_FIELDS_PARTECIPAZIONECORSOBASE
     extra = 0
 
 
-class InlineLezioneCorsoBase(admin.TabularInline):
+class InlineLezioneCorsoBase(ReadonlyAdminMixin, admin.TabularInline):
     model = LezioneCorsoBase
     raw_id_fields = RAW_ID_FIELDS_LEZIONECORSOBASE
     extra = 0
 
 
-class InlineAssenzaCorsoBase(admin.TabularInline):
+class InlineAssenzaCorsoBase(ReadonlyAdminMixin, admin.TabularInline):
     model = AssenzaCorsoBase
     raw_id_fields = RAW_ID_FIELDS_ASSENZACORSOBASE
     extra = 0
@@ -39,7 +41,7 @@ admin_corsi_base_attivi_invia_messaggi.short_description = "(Solo corsi attivi) 
 
 
 @admin.register(CorsoBase)
-class AdminCorsoBase(admin.ModelAdmin):
+class AdminCorsoBase(ReadonlyAdminMixin, admin.ModelAdmin):
     search_fields = ['sede__nome', 'sede__genitore__nome', 'progressivo', 'anno', ]
     list_display = ['progressivo', 'anno', 'stato', 'sede', 'data_inizio', 'data_esame', ]
     list_filter = ['anno', 'creazione', 'stato', 'data_inizio', ]
@@ -49,7 +51,7 @@ class AdminCorsoBase(admin.ModelAdmin):
 
 
 @admin.register(PartecipazioneCorsoBase)
-class AdminPartecipazioneCorsoBase(admin.ModelAdmin):
+class AdminPartecipazioneCorsoBase(ReadonlyAdminMixin, admin.ModelAdmin):
     search_fields = ['persona__nome', 'persona__cognome', 'persona__codice_fiscale', 'corso__progressivo', ]
     list_display = ['persona', 'corso', 'esito', 'creazione', ]
     raw_id_fields = RAW_ID_FIELDS_PARTECIPAZIONECORSOBASE
@@ -57,7 +59,7 @@ class AdminPartecipazioneCorsoBase(admin.ModelAdmin):
 
 
 @admin.register(LezioneCorsoBase)
-class AdminLezioneCorsoBase(admin.ModelAdmin):
+class AdminLezioneCorsoBase(ReadonlyAdminMixin, admin.ModelAdmin):
     search_fields = ['nome', 'corso__progressivo', 'corso__sede__nome', ]
     list_display = ['corso', 'nome', 'inizio', 'fine', ]
     raw_id_fields = RAW_ID_FIELDS_LEZIONECORSOBASE
@@ -65,7 +67,7 @@ class AdminLezioneCorsoBase(admin.ModelAdmin):
 
 
 @admin.register(AssenzaCorsoBase)
-class AdminAssenzaCorsoBase(admin.ModelAdmin):
+class AdminAssenzaCorsoBase(ReadonlyAdminMixin, admin.ModelAdmin):
     search_fields = ['persona__nome', 'persona__cognome', 'persona__codice_fiscale', 'lezione__corso__progressivo',
                      'lezione__corso__sede__nome']
     list_display = ['persona', 'lezione', 'creazione', ]
@@ -78,7 +80,7 @@ def ricalcola_raggio(modeladmin, request, queryset):
 ricalcola_raggio.short_description = "Ricalcola il raggio per gli aspiranti selezionati"
 
 @admin.register(Aspirante)
-class AdminAspirante(admin.ModelAdmin):
+class AdminAspirante(ReadonlyAdminMixin, admin.ModelAdmin):
     search_fields = ['persona__nome', 'persona__cognome', 'persona__codice_fiscale']
     list_display = ['persona', 'creazione', ]
     raw_id_fields = RAW_ID_FIELDS_ASPIRANTE
--- jorvik/settings.py
+++ jorvik/settings.py
@@ -214,7 +214,7 @@ DESTINATARI_REPORT = ['sviluppo@cri.it', 'info@gaia.cri.it']
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             "context_processors": (
