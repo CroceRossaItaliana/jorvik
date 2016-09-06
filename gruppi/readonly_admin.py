ff96477a223a60e3222c52c42baa3e0c02d05ff7
Paolo Romolini <p.romolini@nephila.it>
2016-09-06 17:04:44 +0200

    Readonly model admin aggiornato

--- gruppi/readonly_admin.py
+++ gruppi/readonly_admin.py
@@ -1,11 +1,17 @@
-from django.conf import settings
+from django.contrib.auth import get_permission_codename
+
 
 class ReadonlyAdminMixin(object):
     def get_readonly_fields(self, request, obj=None):
-        if request.user.groups.filter(name=settings.READONLY_GROUP).exists():
-            return list(set(
-                [field.name for field in self.opts.local_fields] +
-                [field.name for field in self.opts.local_many_to_many]
-            ))
+        if obj:
+            view_permission = obj._meta.app_label + '.' + get_permission_codename('view', obj.__class__._meta)
+            add_permission = obj._meta.app_label + '.' + get_permission_codename('add', obj.__class__._meta)
+            if request.user.has_perm(view_permission) and not request.user.has_perm(add_permission):
+                return list(set(
+                    [field.name for field in self.opts.local_fields] +
+                    [field.name for field in self.opts.local_many_to_many]
+                ))
+            else:
+                return self.readonly_fields
         else:
             return self.readonly_fields
