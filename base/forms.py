3dab49da653a550c4f998235d19b74a414016e25
Iacopo Spalletti <i.spalletti@nephila.it>
2017-01-14 12:13:24 +0100

    Selezione persone in form di supporto

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.forms import Textarea
 from django_countries.fields import LazyTypedChoiceField
 from django_countries import countries
 
@@ -25,30 +24,3 @@ class ModuloLocalizzatore(forms.Form):
     comune = forms.CharField(help_text="es. Cinisello Balsamo.")
     provincia = forms.CharField(required=True, min_length=3, help_text="es. Milano. (per intero)")
     stato = LazyTypedChoiceField(choices=countries, initial="IT")
-
-
-class ModuloRichiestaSupporto(forms.Form):
-
-    PRIMO_LIVELLO = "INF"
-    SECONDO_LIVELLO = "REQ"
-    TERZO_LIVELLO = "INC"
-    FEEDBACK = "FEE"
-    SANGUE = "BLO"
-    AREA_SVILUPPO = "SVI"
-    TIPO = (
-        (None, "-- Seleziona una opzione --"),
-        (PRIMO_LIVELLO, "Informazione: Aiuto con l'utilizzo di Gaia"),
-        (SECONDO_LIVELLO, "Richiesta: Modifica informazioni o correzioni"),
-        (TERZO_LIVELLO, "Incidente: Errori o segnalazioni di sicurezza"),
-        (AREA_SVILUPPO, "Area VI: Ripristino password e richieste e-mail istituzionali (@cri.it, PEC)"),
-        (FEEDBACK, "Feedback GAIA (suggerimenti, critiche, idee)"),
-        (SANGUE, "Feedback in merito alla donazione sangue"),
-
-    )
-
-    tipo = forms.ChoiceField(TIPO, required=True, initial=None,
-                                   help_text="Seleziona una delle tipologie di richiesta "
-                                             "per aiutarci a smistarla rapidamente.")
-
-    oggetto = forms.CharField(help_text="Una breve descrizione del problema, comprensiva del codice fiscale del volontario per cui richiedi assistenza.")
-    descrizione = forms.CharField(widget=Textarea)
