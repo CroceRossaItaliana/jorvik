2fcefe2a92c1e1b515d7a3fa2eff210d8da5b1a7
Andrea Carmisciano <andrea.carmisciano@citelgroup.it>
2018-03-22 23:13:49 +0100

    posta: switch completo a celery con metodi di compatibilità

--- posta/models.py
+++ posta/models.py
@@ -6,8 +6,6 @@ from smtplib import SMTPException, SMTPRecipientsRefused
 
 from django.core.mail import EmailMultiAlternatives, get_connection
 from django.db import transaction, DatabaseError
-from django.db.models import QuerySet
-from django.template.loader import get_template
 from django.utils.html import strip_tags
 from lxml import html
 
@@ -53,6 +51,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
     eliminato = models.BooleanField(default=False, null=False)
 
     priorita = models.IntegerField(default=0)
+    utenza = models.BooleanField(default=False)
 
     @property
     def destinatari(self):
@@ -91,7 +90,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
         #    return self.corpo
         #print html.parse('http://someurl.at.domain').xpath('//body')[0].text_content()
 
-    def _processa_link(self):
+    def processa_link(self):
         """
         Controlla i link nella e-mail relativi e li rende assoluti.
         """
@@ -121,7 +120,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
             logger.debug(*args, **kwargs)
 
     @staticmethod
-    def invia(pk, connection=None, utenza=None):
+    def invia(pk):
         with transaction.atomic():
             try:
                 messaggio = Messaggio.objects.select_for_update(nowait=True).get(pk=pk, terminato__isnull=True)
@@ -140,7 +139,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
             mittente = '{} <{}>'.format(nome_mittente, Messaggio.NOREPLY_EMAIL)
             plain_text = strip_tags(messaggio.corpo)
 
-            connection = connection or get_connection()
+            connection = get_connection()
 
             # non metto filter su inviato altrimenti non posso distiguere quelle senza destinatari
             destinatari = messaggio.oggetti_destinatario.all()
@@ -150,7 +149,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
                     continue
 
                 mail_to = [d.persona.email]
-                if utenza:
+                if messaggio.utenza:
                     email_utenza = getattr(getattr(d.persona, 'utenza', {}), 'email', None)
                     if email_utenza != d.persona.email:
                         mail_to.append(email_utenza)
@@ -188,7 +187,7 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
                     subject=messaggio.oggetto,
                     body=plain_text,
                     from_email=mittente,
-                    reply_to=[reply_to],
+                    reply_to=reply_to,
                     to=[Messaggio.SUPPORTO_EMAIL],
                     attachments=messaggio.allegati_pronti(),
                     connection=connection)
@@ -208,16 +207,6 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
         if not messaggio.oggetti_destinatario.filter(inviato=False):
             messaggio.terminato = messaggio.ultimo_tentativo
         messaggio.save()
-
-    def aggiungi_destinatario(self, persona):
-        """
-        Aggiunge un destinatario al messaggio di posta.
-        Il messaggio DEVE essere salvato.
-        :param persona: La Persona da aggiungere.
-        :return:
-        """
-        d = Destinatario(messaggio=self, persona=persona)
-        return d.save()
 
     @classmethod
     def in_coda(cls):
@@ -251,80 +240,9 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
             msg.attach_file(allegato.file.path)
         return msg.send(fail_silently=fallisci_silenziosamente)
 
-    @classmethod
-    def smaltisci_coda(cls, dimensione_massima=50):
-        da_smaltire = cls.in_coda()[:dimensione_massima]
-        print("%s == ricerca messaggi da smaltire, coda=%d" % (
-            datetime.now().isoformat(' '),
-            dimensione_massima,
-        ))
-        totale = 0
-        print("%s apro connessione al backed di invio posta" % (
-            datetime.now().isoformat(' '),
-        ))
-        connection = get_connection()
-        for messaggio in da_smaltire:
-            totale += 1
-            if cls.logging:
-                logger.debug('MSG %s: pickup destinatari=(totale=%d, in_attesa=%d)' % (
-                    messaggio.pk, messaggio.oggetti_destinatario.all().count(),
-                    messaggio.oggetti_destinatario.filter(inviato=False).count(),
-                ))
-            print("%s invio messaggio id=%d, destinatari=(totale=%d, in_attesa=%d)" % (
-                datetime.now().isoformat(' '),
-                messaggio.pk,
-                messaggio.oggetti_destinatario.all().count(),
-                messaggio.oggetti_destinatario.filter(inviato=False).count(),
-            ))
-            messaggio.invia(connection)
-        print("%s -- fine elaborazione, elaborati=%d" % (
-            datetime.now().isoformat(' '),
-            totale,
-        ))
-
-    @classmethod
-    def costruisci(cls, oggetto='Nessun oggetto', modello='email_vuoto.html', corpo=None, mittente=None,
-                   destinatari=None, allegati=None, **kwargs):
-
-        corpo = corpo or {}
-        destinatari = destinatari or []
-        allegati = allegati or []
-
-        corpo.update({
-            "mittente": mittente,
-            "allegati": allegati,
-        })
-
-        # Accorcia l'oggetto se necessario.
-        if len(oggetto) > cls.LUNGHEZZA_MASSIMA_OGGETTO:
-            oggetto = oggetto[:(cls.LUNGHEZZA_MASSIMA_OGGETTO - len(cls.CARATTERI_RIDUZIONE_OGGETTTO))]
-            oggetto = "%s%s" % (oggetto, cls.CARATTERI_RIDUZIONE_OGGETTTO)
-
-        m = cls(
-            oggetto=oggetto,
-            mittente=mittente,
-            corpo=get_template(modello).render(corpo),
-            **kwargs
-        )
-        m._processa_link()
-        m.save()
-
-        # Se QuerySet, rendi distinti.
-        if isinstance(destinatari, QuerySet):
-            destinatari = destinatari.distinct()
-
-        for d in destinatari:
-            m.aggiungi_destinatario(d)
-
-        for a in allegati:
-            a.oggetto = m
-            a.save()
-
-        return m
-
-    @classmethod
-    def costruisci_e_invia(cls, oggetto='Nessun oggetto', modello='email_vuoto.html', corpo=None, mittente=None,
-                           destinatari=None, allegati=None, utenza=False, **kwargs):
+    @staticmethod
+    def costruisci_e_invia(oggetto='Nessun oggetto', modello='email_vuoto.html', corpo=None, mittente=None,
+                           destinatari=None, allegati=None, **kwargs):
         """
         Scorciatoia per costruire rapidamente un messaggio di posta e inviarlo immediatamente.
          IMPORTANTE. Non adatto per messaggi con molti destinatari. In caso di fallimento immediato, il messaggio
@@ -337,28 +255,17 @@ class Messaggio(ModelloSemplice, ConMarcaTemporale, ConGiudizio, ConAllegati):
         :param allegati: Allegati messaggio
         :return: Un oggetto Messaggio inviato.
         """
-        m = cls.costruisci(oggetto=oggetto, modello=modello, corpo=corpo, mittente=mittente, destinatari=destinatari,
-                           allegati=allegati, **kwargs)
-        m.invia(utenza=utenza)
-        return m
 
-    @classmethod
-    def costruisci_e_accoda(cls, oggetto='Nessun oggetto', modello='email_vuoto.html', corpo=None, mittente=None,
-                            destinatari=None, allegati=None, **kwargs):
-        """
-        Scorciatoia per costruire rapidamente un messaggio di posta e accodarlo per l'invio asincrono.
-        :param oggetto: Oggetto del messaggio.
-        :param modello: Modello da utilizzare per l'invio.
-        :param corpo: Sostituzioni da fare nel modello. Dizionario {nome: valore}
-        :param mittente: Mittente del messaggio. None per Notifiche da Gaia.
-        :param destinatari: Un elenco di destinatari (oggetti Persona).
-        :param allegati: Allegati del messaggio
-        :return: Un oggetto Messaggio accodato.
-        """
-        m = cls.costruisci(oggetto=oggetto, modello=modello, corpo=corpo, mittente=mittente, destinatari=destinatari,
-                           allegati=allegati, **kwargs)
-        m.accoda()
-        return m
+        from .tasks import crea_email
+        destinatari = destinatari or []
+        destinatari = set([d.pk for d in destinatari])
+        allegati = allegati or []
+        allegati = set([a.pk for a in allegati])
+
+        crea_email(oggetto=oggetto, modello=modello, corpo=corpo, mittente=mittente, destinatari=destinatari,
+                   allegati=allegati, **kwargs)
+
+    costruisci_e_accoda = costruisci_e_invia
 
     def __str__(self):
         return self.oggetto
