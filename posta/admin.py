e8ad645b663b19c4a5831f89a2f04eec26343392
Andrea Carmisciano <andrea.carmisciano@citelgroup.it>
2018-03-26 21:16:37 +0200

    posta: riaggiunto modello admin Destinatario

--- posta/admin.py
+++ posta/admin.py
@@ -22,3 +22,12 @@ class AdminMessaggio(ReadonlyAdminMixin, admin.ModelAdmin):
     list_filter = ('creazione', 'terminato', 'ultimo_tentativo',)
     raw_id_fields = ('mittente', 'rispondi_a')
     inlines = [AdminDestinatarioInline]
+
+
+@admin.register(Destinatario)
+class AdminDestinatario(ReadonlyAdminMixin, admin.ModelAdmin):
+    search_fields = ['messaggio__oggetto', 'persona__codice_fiscale', 'persona__email_contatto',
+                     'persona__utenza__email', 'errore',]
+    list_display = ('messaggio', 'persona', 'inviato', 'tentativo', 'errore')
+    list_filter = ('inviato', 'tentativo', )
+    raw_id_fields = ('persona', 'messaggio',)
