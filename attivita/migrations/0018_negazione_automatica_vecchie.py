be2cb93ab87cea7f19d8487f7911b8d7481c0f9f
tvcitel <37589887+tvcitel@users.noreply.github.com>
2018-04-20 14:14:24 +0200

    JO-316: complessità codice e query
    Data la complessità e il gran numero di query che sarebbero state effettuate con il codice precedente, si è proceduto con una semplificazione del codice riducendo sensibilmente il numero di query.

--- attivita/migrations/0018_negazione_automatica_vecchie.py
+++ attivita/migrations/0018_negazione_automatica_vecchie.py
@@ -4,38 +4,20 @@ from __future__ import unicode_literals
 from datetime import datetime, date, timedelta
 
 from django.db import migrations, models
+from anagrafica.permessi.incarichi import INCARICO_GESTIONE_ATTIVITA_PARTECIPANTI
 
 
 def nega_partecipazioni_vecchie(apps, schema_editor):
 
-    Partecipazione = apps.get_model("attivita", "Partecipazione")
     Autorizzazione = apps.get_model("base", "Autorizzazione")
 
-    partecipzioni_pendenti = Partecipazione.objects.filter(confermata=False, ritirata=False, automatica=False)
-    totale = partecipzioni_pendenti.count()
-    print("  => 0018 trovate %d partecipazioni di attivita pendenti" % (totale))
+    # Autorizzazioni attività di partecipazione, senza esito ed escludendo quelle già negate.
+    autorizzazioni = Autorizzazione.objects.filter(concessa__isnull=True,
+                                                   destinatario_ruolo=INCARICO_GESTIONE_ATTIVITA_PARTECIPANTI
+                                                  ).exclude(tipo_gestione="N")
 
-    autorizzazioni_con_scadenza = 0
-    autorizzazioni_senza_scadenza = 0
-    for partecipazione in partecipzioni_pendenti:
-        # Autorizzazioni senza esito della partecipazione pendente
-        autorizzazioni_partecipazione = Autorizzazione.objects.filter(oggetto_id=partecipazione.pk, concessa=None)
-        for autorizzazione in autorizzazioni_partecipazione:
-            # Se l'autorizzazione ha una scadenza
-            if autorizzazione.scadenza:
-                # Imposta negazione automatica per autorizzazione con scadenza
-                if(autorizzazione.tipo_gestione != 'N'):
-                    autorizzazione.tipo_gestione = 'N'
-                    autorizzazione.save()
-                    autorizzazioni_con_scadenza += 1
-            else:
-                # Imposta negazione automatica e scadenza 30gg per autorizzazione senza scadenza
-                autorizzazione.tipo_gestione = 'N'
-                autorizzazione.scadenza = autorizzazione.creazione + timedelta(days=30)
-                autorizzazione.save()
-                autorizzazioni_senza_scadenza += 1
-
-    print("  ==> 0018 modificate %d autorizzazioni con scadenza e %d autorizzazioni senza scadenza" % (autorizzazioni_con_scadenza, autorizzazioni_senza_scadenza))
+    # Impostazione scadenza su data attuale e negazione automatica.
+    autorizzazioni.update(scadenza=datetime.now(), tipo_gestione="N")
 
 
 class Migration(migrations.Migration):
