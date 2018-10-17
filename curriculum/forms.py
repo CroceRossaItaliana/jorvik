3eb52240d47172129aee6a3186a49b23ed7a6c6b
Arkady Karlkvist <austern1@gmail.com>
2018-10-17 15:09:45 +0200

    ADDED: (in curriculum.forms.ModuloNuovoTitoloPersonale) validation of titolo field to avoid adding same fields multiple times.

--- curriculum/forms.py
+++ curriculum/forms.py
@@ -10,7 +10,22 @@ class ModuloNuovoTitoloPersonale(autocomplete_light.ModelForm):
         model = TitoloPersonale
         fields = ['titolo',]
 
+    def clean_titolo(self):
+        titolo = self.cleaned_data['titolo']
+        me = self.me
+        
+        # Prevent adding the same titles multiple times
+        confirmed_titles = me.titoli_confermati()
+        if titolo in confirmed_titles:
+            raise forms.ValidationError(
+                "Hai già inserito questo titolo nel tuo curriculum.")
+        
+        return titolo
+
     def __init__(self, tipo, tipo_display, *args, **kwargs):
+        # Needs access to the object in clean_titolo()
+        # (stored in .views.utente_curriculum)
+        self.me = kwargs.pop('me', None)
         super().__init__(*args, **kwargs)
         
         self.fields['titolo'].label = tipo_display
@@ -45,7 +60,8 @@ class ModuloNuovoTitoloPersonale(autocomplete_light.ModelForm):
 class ModuloDettagliTitoloPersonale(forms.ModelForm):
     class Meta:
         model = TitoloPersonale
-        fields = ['data_ottenimento', 'luogo_ottenimento', 'data_scadenza', 'codice',]
+        fields = ['data_ottenimento', 'luogo_ottenimento', 'data_scadenza',
+                  'codice',]
 
     def __init__(self, *args, **kwargs):
         super(ModuloDettagliTitoloPersonale, self).__init__(*args, **kwargs)
