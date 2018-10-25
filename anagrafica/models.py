efdc1c6482a5599bb6fc7a401fa21694127d5ed8
Arkady Karlkvist <austern1@gmail.com>
2018-10-25 17:05:05 +0200

    FIX: imports re-ordering, following 80-characters per line rule.

--- anagrafica/models.py
+++ anagrafica/models.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-
 """
 Questo modulo definisce i modelli del modulo anagrafico di Gaia.
 
@@ -11,60 +10,61 @@ Questo modulo definisce i modelli del modulo anagrafico di Gaia.
 - Comitato
 - Delega
 """
-from datetime import date, timedelta, datetime
 
+from datetime import date, timedelta, datetime
 import stdnum
 from django.conf import settings
+from django.apps import apps
+from django.db import models
+from django.db.models import Q, QuerySet, Avg
 from django.db.transaction import atomic
 from django.utils import timezone
-
-import codicefiscale
-import mptt
-from django.contrib.auth.models import PermissionsMixin
+from django.utils.functional import cached_property
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
-from django.db import models
-from django.db.models import Q, QuerySet, Avg
-from django.utils.functional import cached_property
-from django_countries.fields import CountryField
+
 import phonenumbers
+import codicefiscale
+import mptt
 from mptt.querysets import TreeQuerySet
-
-from anagrafica.costanti import ESTENSIONE, TERRITORIALE, LOCALE, PROVINCIALE, REGIONALE, NAZIONALE
-
-from anagrafica.permessi.applicazioni import PRESIDENTE, PERMESSI_NOMI, PERMESSI_NOMI_DICT, UFFICIO_SOCI_UNITA, \
-    DELEGHE_RUBRICA, DELEGATO_OBIETTIVO_2, DELEGATO_OBIETTIVO_3, DELEGATO_OBIETTIVO_1, DELEGATO_OBIETTIVO_4, \
-    RESPONSABILE_FORMAZIONE, DELEGATO_OBIETTIVO_6, DELEGATO_OBIETTIVO_5, RESPONSABILE_AUTOPARCO, DELEGATO_CO, \
-    DIRETTORE_CORSO, RESPONSABILE_AREA, REFERENTE, OBIETTIVI, COMMISSARIO
-from anagrafica.permessi.applicazioni import UFFICIO_SOCI
-from anagrafica.permessi.costanti import GESTIONE_ATTIVITA, PERMESSI_OGGETTI_DICT, GESTIONE_SOCI, GESTIONE_CORSI_SEDE, GESTIONE_CORSO, \
-    GESTIONE_SEDE, GESTIONE_AUTOPARCHI_SEDE, GESTIONE_CENTRALE_OPERATIVA_SEDE
-from anagrafica.permessi.delega import delega_permessi, delega_incarichi
-from anagrafica.permessi.incarichi import INCARICO_GESTIONE_APPARTENENZE, INCARICO_GESTIONE_TRASFERIMENTI, \
-    INCARICO_GESTIONE_ESTENSIONI, INCARICO_GESTIONE_RISERVE, INCARICO_ASPIRANTE
-from anagrafica.permessi.persona import persona_ha_permesso, persona_oggetti_permesso, persona_permessi, \
-    persona_permessi_almeno, persona_ha_permessi
-from anagrafica.validators import valida_codice_fiscale, ottieni_genere_da_codice_fiscale, \
-    crea_validatore_dimensione_file, valida_dimensione_file_8mb, valida_dimensione_file_5mb, valida_almeno_14_anni, \
-    valida_partita_iva, valida_iban, valida_email_personale
-from attivita.models import Turno, Partecipazione
-from base.files import PDF, Excel, FoglioExcel
-from base.geo import ConGeolocalizzazioneRaggio, ConGeolocalizzazione
-from base.models import ModelloSemplice, ModelloAlbero, ConAutorizzazioni, ConAllegati, \
-    Autorizzazione, ConVecchioID
-from base.stringhe import normalizza_nome, GeneratoreNomeFile
-from base.tratti import ConMarcaTemporale, ConStorico, ConProtocollo, ConDelegati, ConPDF
-from base.utils import is_list, sede_slugify, UpperCaseCharField, TitleCharField, poco_fa, mezzanotte_24_ieri, \
-    mezzanotte_00, mezzanotte_24, concept
+from django_countries.fields import CountryField
 from autoslug import AutoSlugField
 
+from attivita.models import Turno, Partecipazione
 from curriculum.models import Titolo, TitoloPersonale
 from posta.models import Messaggio
-from django.apps import apps
-
 from base.notifiche import NOTIFICA_INVIA, NOTIFICA_NON_INVIARE
+from base.files import PDF, Excel, FoglioExcel
+from base.geo import ConGeolocalizzazioneRaggio, ConGeolocalizzazione
+from base.models import (ModelloSemplice, ModelloAlbero, ConAutorizzazioni,
+    ConAllegati, Autorizzazione, ConVecchioID)
+from base.stringhe import normalizza_nome, GeneratoreNomeFile
+from base.tratti import ConMarcaTemporale, ConStorico, ConProtocollo, ConDelegati, ConPDF
+from base.utils import (is_list, sede_slugify, UpperCaseCharField,
+    TitleCharField, poco_fa, mezzanotte_24_ieri, mezzanotte_00, mezzanotte_24, concept)
+from anagrafica.permessi.delega import delega_permessi, delega_incarichi
+from anagrafica.costanti import (ESTENSIONE, TERRITORIALE, LOCALE,
+                                 PROVINCIALE, REGIONALE, NAZIONALE)
+from anagrafica.permessi.applicazioni import (PRESIDENTE, PERMESSI_NOMI,
+    PERMESSI_NOMI_DICT, UFFICIO_SOCI_UNITA, DELEGHE_RUBRICA, DELEGATO_OBIETTIVO_2,
+    DELEGATO_OBIETTIVO_3, DELEGATO_OBIETTIVO_1, DELEGATO_OBIETTIVO_4,
+    RESPONSABILE_FORMAZIONE, DELEGATO_OBIETTIVO_6, DELEGATO_OBIETTIVO_5,
+    RESPONSABILE_AUTOPARCO, DELEGATO_CO, DIRETTORE_CORSO, RESPONSABILE_AREA,
+    REFERENTE, OBIETTIVI, COMMISSARIO, UFFICIO_SOCI)
+from anagrafica.permessi.costanti import (GESTIONE_ATTIVITA,
+    PERMESSI_OGGETTI_DICT, GESTIONE_SOCI, GESTIONE_CORSI_SEDE, GESTIONE_CORSO,
+    GESTIONE_SEDE, GESTIONE_AUTOPARCHI_SEDE, GESTIONE_CENTRALE_OPERATIVA_SEDE)
+from anagrafica.permessi.incarichi import (INCARICO_GESTIONE_APPARTENENZE,
+    INCARICO_GESTIONE_TRASFERIMENTI, INCARICO_GESTIONE_ESTENSIONI,
+    INCARICO_GESTIONE_RISERVE, INCARICO_ASPIRANTE)
+from anagrafica.permessi.persona import (persona_ha_permesso,
+    persona_oggetti_permesso, persona_permessi, persona_permessi_almeno,
+    persona_ha_permessi)
+from anagrafica.validators import (valida_codice_fiscale, valida_iban,
+    ottieni_genere_da_codice_fiscale, crea_validatore_dimensione_file,
+    valida_dimensione_file_8mb, valida_dimensione_file_5mb,
+    valida_almeno_14_anni, valida_partita_iva, valida_email_personale)
 
 
 class Persona(ModelloSemplice, ConMarcaTemporale, ConAllegati, ConVecchioID):
