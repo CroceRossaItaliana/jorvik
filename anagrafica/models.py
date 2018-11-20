9e4a82e90df250faa2fc7b72861ce3e899102431
Arkady Karlkvist <austern1@gmail.com>
2018-11-20 15:33:11 +0100

    ADDED: Persona.to_contact_for_courses class-method; re-ordered imports.

--- anagrafica/models.py
+++ anagrafica/models.py
@@ -1,70 +1,63 @@
-# coding=utf-8
-
-"""
-Questo modulo definisce i modelli del modulo anagrafico di Gaia.
-
-- Persona
-- Telefono
-- Documento
-- Utente
-- Appartenenza
-- Comitato
-- Delega
-"""
 from datetime import date, timedelta, datetime
 
-import stdnum
-from django.conf import settings
+# from django.conf import settings
 from django.db.transaction import atomic
 from django.utils import timezone
 
+# import stdnum
 import codicefiscale
+import phonenumbers
 import mptt
-from django.contrib.auth.models import PermissionsMixin
+from mptt.querysets import TreeQuerySet
+
+# from django.contrib.auth.models import PermissionsMixin
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
+from django.apps import apps
 from django.db import models
 from django.db.models import Q, QuerySet, Avg
 from django.utils.functional import cached_property
 from django_countries.fields import CountryField
-import phonenumbers
-from mptt.querysets import TreeQuerySet
 
-from anagrafica.costanti import ESTENSIONE, TERRITORIALE, LOCALE, PROVINCIALE, REGIONALE, NAZIONALE
-
-from anagrafica.permessi.applicazioni import PRESIDENTE, PERMESSI_NOMI, PERMESSI_NOMI_DICT, UFFICIO_SOCI_UNITA, \
-    DELEGHE_RUBRICA, DELEGATO_OBIETTIVO_2, DELEGATO_OBIETTIVO_3, DELEGATO_OBIETTIVO_1, DELEGATO_OBIETTIVO_4, \
-    RESPONSABILE_FORMAZIONE, DELEGATO_OBIETTIVO_6, DELEGATO_OBIETTIVO_5, RESPONSABILE_AUTOPARCO, DELEGATO_CO, \
-    DIRETTORE_CORSO, RESPONSABILE_AREA, REFERENTE, OBIETTIVI, COMMISSARIO
-from anagrafica.permessi.applicazioni import UFFICIO_SOCI
-from anagrafica.permessi.costanti import GESTIONE_ATTIVITA, PERMESSI_OGGETTI_DICT, GESTIONE_SOCI, GESTIONE_CORSI_SEDE, GESTIONE_CORSO, \
-    GESTIONE_SEDE, GESTIONE_AUTOPARCHI_SEDE, GESTIONE_CENTRALE_OPERATIVA_SEDE
-from anagrafica.permessi.delega import delega_permessi, delega_incarichi
-from anagrafica.permessi.incarichi import INCARICO_GESTIONE_APPARTENENZE, INCARICO_GESTIONE_TRASFERIMENTI, \
-    INCARICO_GESTIONE_ESTENSIONI, INCARICO_GESTIONE_RISERVE, INCARICO_ASPIRANTE
-from anagrafica.permessi.persona import persona_ha_permesso, persona_oggetti_permesso, persona_permessi, \
-    persona_permessi_almeno, persona_ha_permessi
-from anagrafica.validators import valida_codice_fiscale, ottieni_genere_da_codice_fiscale, \
-    crea_validatore_dimensione_file, valida_dimensione_file_8mb, valida_dimensione_file_5mb, valida_almeno_14_anni, \
-    valida_partita_iva, valida_iban, valida_email_personale
+from .costanti import (ESTENSIONE, TERRITORIALE, LOCALE, PROVINCIALE,
+                       REGIONALE, NAZIONALE)
+from .permessi.applicazioni import (PRESIDENTE, PERMESSI_NOMI, COMMISSARIO,
+    PERMESSI_NOMI_DICT, UFFICIO_SOCI_UNITA, DELEGHE_RUBRICA, OBIETTIVI,
+    DELEGATO_OBIETTIVO_2, DELEGATO_OBIETTIVO_3, DELEGATO_OBIETTIVO_1,
+    REFERENTE, DELEGATO_OBIETTIVO_4, RESPONSABILE_FORMAZIONE,
+    DELEGATO_OBIETTIVO_6, DELEGATO_OBIETTIVO_5, RESPONSABILE_AUTOPARCO,
+    DELEGATO_CO, DIRETTORE_CORSO, RESPONSABILE_AREA)
+from .permessi.applicazioni import UFFICIO_SOCI
+from .permessi.costanti import (GESTIONE_ATTIVITA, PERMESSI_OGGETTI_DICT,
+    GESTIONE_SOCI, GESTIONE_CORSI_SEDE, GESTIONE_CORSO, GESTIONE_SEDE,
+    GESTIONE_AUTOPARCHI_SEDE, GESTIONE_CENTRALE_OPERATIVA_SEDE)
+from .permessi.delega import delega_permessi, delega_incarichi
+from .permessi.incarichi import (INCARICO_GESTIONE_APPARTENENZE,
+    INCARICO_GESTIONE_TRASFERIMENTI, INCARICO_GESTIONE_ESTENSIONI,
+    INCARICO_GESTIONE_RISERVE, INCARICO_ASPIRANTE)
+from anagrafica.permessi.persona import (persona_ha_permesso,
+    persona_oggetti_permesso, persona_permessi, persona_permessi_almeno,
+    persona_ha_permessi)
+from .validators import (valida_codice_fiscale, ottieni_genere_da_codice_fiscale,
+    crea_validatore_dimensione_file, valida_dimensione_file_8mb,
+    valida_dimensione_file_5mb, valida_almeno_14_anni, valida_partita_iva,
+    valida_iban, valida_email_personale)
 from attivita.models import Turno, Partecipazione
 from base.files import PDF, Excel, FoglioExcel
 from base.geo import ConGeolocalizzazioneRaggio, ConGeolocalizzazione
-from base.models import ModelloSemplice, ModelloAlbero, ConAutorizzazioni, ConAllegati, \
-    Autorizzazione, ConVecchioID
+from base.models import (ModelloSemplice, ModelloAlbero, ConAutorizzazioni,
+    ConAllegati, Autorizzazione, ConVecchioID)
 from base.stringhe import normalizza_nome, GeneratoreNomeFile
-from base.tratti import ConMarcaTemporale, ConStorico, ConProtocollo, ConDelegati, ConPDF
-from base.utils import is_list, sede_slugify, UpperCaseCharField, TitleCharField, poco_fa, mezzanotte_24_ieri, \
-    mezzanotte_00, mezzanotte_24, concept
+from base.tratti import (ConMarcaTemporale, ConStorico, ConProtocollo,
+                         ConDelegati, ConPDF)
+from base.utils import (is_list, sede_slugify, UpperCaseCharField, concept,
+    TitleCharField, poco_fa, mezzanotte_24_ieri, mezzanotte_00, mezzanotte_24)
+# from base.notifiche import NOTIFICA_INVIA, NOTIFICA_NON_INVIARE
 from autoslug import AutoSlugField
-
 from curriculum.models import Titolo, TitoloPersonale
 from posta.models import Messaggio
-from django.apps import apps
 
-from base.notifiche import NOTIFICA_INVIA, NOTIFICA_NON_INVIARE
 
 
 class Persona(ModelloSemplice, ConMarcaTemporale, ConAllegati, ConVecchioID):
@@ -1276,6 +1269,33 @@ class Persona(ModelloSemplice, ConMarcaTemporale, ConAllegati, ConVecchioID):
                         attivi.append({'segmento': segmento, 'sede': sede})
                         attivi.append({'sedi_sottostanti': True, 'sede': sede.genitore})
         return attivi
+
+    @classmethod
+    @concept
+    def to_contact_for_courses(cls, corso, membro='VO', *args, **kwargs):
+        from formazione.models import Corso, PartecipazioneCorsoBase
+
+        if membro == Appartenenza.VOLONTARIO:
+            # Exclude persons that have already made participation request
+            # to <corso> as <VOLONTARIO> and the request has been <confirmed>
+
+            to_exclude = cls.objects.filter(
+                PartecipazioneCorsoBase.con_esito(
+                    PartecipazioneCorsoBase.ESITO_OK,
+                    corso__tipo=Corso.CORSO_NUOVO,
+                    corso=corso
+                ).via("partecipazioni_corsi")
+            )
+
+            without_to_exclude = ~Q(id__in=to_exclude.values_list('id', flat=True))
+            return Q(without_to_exclude, *args, **kwargs)
+
+        elif membro == Appartenenza.ESTESO:
+            # TODO: place for another Appartenenza membro
+            pass
+        else:
+            # TODO: what to return otherwise?
+            pass
 
     def save(self, *args, **kwargs):
         self.nome = normalizza_nome(self.nome)
