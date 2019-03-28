bd7ef327d61c774095e5fd98ed8b0c125d86e714
Arkady Karlkvist <austern1@gmail.com>
2019-03-28 10:57:10 +0100

    Revert "hotfix elenchi"
    This reverts commit ecdb42a4df638c61837e498f92d573d9ec89343f.

--- ufficio_soci/reports.py
+++ ufficio_soci/reports.py
@@ -357,6 +357,6 @@ class ReportElencoSoci:
             # If the report is ready, download it without redirect user
             return report_db.download()
         else:
-            response = None #redirect(reverse('elenchi_richiesti_download'))
+            response = redirect(reverse('elenchi_richiesti_download'))
             messages.success(self.request, 'Attendi la generazione del report richiesto.')
             return response
