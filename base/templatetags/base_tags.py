720f85f56927ee0f0d9372a01a922ac5e1e7de64
Arkady Karlkvist <austern1@gmail.com>
2019-05-22 16:23:29 +0200

    MODIFICATO: scramble per fare le prove mail (And.)

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -15,6 +15,30 @@ def bool(value):
     else:
         return 'false'
 
+@register.simple_tag(takes_context=True)
+def show_test_users_with_credentials(context):
+    from anagrafica.models import Persona
+    from django.utils.safestring import mark_safe
+
+    try:
+        request = context['request']
+        host = request.build_absolute_uri()
+
+        if "provami.gaia.cri.it" in host or "127.0.0.1" in host:
+            vo = Persona.objects.filter(nome__istartswith='volontario_').last()
+            pr = Persona.objects.filter(nome__istartswith='presidente_').last()
+
+            html = ''
+            if vo:
+                html = "<div>%s : %s</div>" % (vo.utenza, vo.utenza.check_password(vo.utenza.email))
+            if pr:
+                html += "<div>%s : %s</div>" % (pr.utenza, pr.utenza.check_password(pr.utenza.email))
+
+            return mark_safe(html)
+    except:
+        # per escludere qualsiasi exception
+        pass
+
 
 @register.filter('startswith')
 def startswith(text, starts):
