1475685503d84d8b33faa858e7c0406de22fcba8
Arkady Karlkvist <austern1@gmail.com>
2019-08-28 14:11:02 +0200

    NEW: campo sigla in Curriculum.Titolo (migration 0017)

--- curriculum/models.py
+++ curriculum/models.py
@@ -41,6 +41,7 @@ class Titolo(ModelloSemplice, ConVecchioID):
     area = models.CharField(max_length=5, null=True, blank=True, db_index=True,
         choices=OBBIETTIVI_STRATEGICI)
     tipo = models.CharField(max_length=2, choices=TIPO, db_index=True)
+    sigla = models.CharField(max_length=50, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     expires_after = models.IntegerField(null=True, blank=True, verbose_name="Scadenza",
