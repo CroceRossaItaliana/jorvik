2c650948582b3b8315af26108fe2404d9109b540
vittorio.dargenio <Vitt_1992>
2019-09-04 16:07:22 +0200

    fix referenti

--- attivita/cri_persone.py
+++ attivita/cri_persone.py
@@ -14,8 +14,8 @@ def getServiziStandard(max_result=200):
 def updateServizio(key, **kwargs):
     data = {}
     if kwargs.get('referenti'):
-        # data['accountables'] = [{'name': '{}.{}'.format(ref.nome, ref.cognome)} for ref in kwargs.get('referenti')]
-        data['accountables'] = [{'name': 'gianluca.silvestri'}]
+        data['accountables'] = [{'name': '{}.{}'.format(ref.nome, ref.cognome)} for ref in kwargs.get('referenti')]
+        # data['accountables'] = [{'name': 'gianluca.silvestri'}]
 
     if kwargs.get('precedenti'):
         data['accountables'].extend(kwargs.get('precedenti'))
