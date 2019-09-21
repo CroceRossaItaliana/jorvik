15854d7f025923c9dc393dddd21f377713b86938
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2019-09-21 15:48:23 +0200

    cleanup

--- anagrafica/tasks.py
+++ anagrafica/tasks.py
@@ -12,10 +12,10 @@ logger = get_task_logger(__name__)
 def prefetch_onlogin(self, pk):
 
     persona = Persona.objects.get(pk=pk)
-    data = prepare_us(persona)
+    us_data = prepare_us(persona)
 
-    cache.set('{}_us_sedi'.format(pk), [s.id for s in data.get('sedi')])
-    cache.set('{}_us_persone'.format(pk), data.get('persone'))
-    cache.set('{}_us_attivi'.format(pk), data.get('attivi'))
+    cache.set('{}_us_sedi'.format(pk), [s.id for s in us_data.get('sedi')])
+    cache.set('{}_us_persone'.format(pk), us_data.get('persone'))
+    cache.set('{}_us_attivi'.format(pk), us_data.get('attivi'))
 
     print(cache.get('{}_us_sedi'.format(pk)))
