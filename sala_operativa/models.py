9b3c8616bec65b2998eedf3554f3fdb8060fce15
Arkady Karlkvist <austern1@gmail.com>
2020-06-11 14:50:44 +0200

    NEW: (GAIA-279) model ReperibitaSO (migr: 0001_initial)

--- sala_operativa/models.py
+++ sala_operativa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from anagrafica.models import Persona
+from base.models import ModelloSemplice
+from base.tratti import ConMarcaTemporale, ConStorico
+
+
+class ReperibilitaSO(ModelloSemplice, ConMarcaTemporale, ConStorico):
+    attivazione = models.TimeField("Tempo di attivazione", default="00:15",
+        help_text="Tempo necessario all'attivazione, in formato HH:mm.",)
+    persona = models.ForeignKey(Persona, related_name="so_reperibilita", on_delete=models.CASCADE)
+
+    @classmethod
+    def reperibilita_di(cls, persona):
+        return cls.objects.filter(persona=persona)
+
+    def __str__(self):
+        return str(self.persona)
+
+    class Meta:
+        verbose_name = "Reperibilità"
+        verbose_name_plural = "Reperibilità"
+        ordering = ["-inizio", "-fine"]
+        permissions = (
+            ("view_reperibilita", "Can view Reperibilità"),
+        )
