e455eb9d4a15da79b6f99815bc875feca7e9bf63
Arkady Karlkvist <austern1@gmail.com>
2020-11-30 11:44:23 +0100

    FUXED: (GAIA-320) dicitura delibera N. numero nel menu

--- formazione/menus.py
+++ formazione/menus.py
@@ -29,8 +29,8 @@ def formazione_menu(menu_name, me=None):
             ('Catalogo Corsi', 'fa-list-alt', reverse('courses:catalog')),
             ('Acronimi', 'fa-book', '/page/glossario-corsi/'),
             ('Regolamento Formazione', 'fa-book', 'https://datafiles.gaia.cri.it/media/filer_public/08/59/0859cd54-ddad-4f26-8f8d-f48d2c92801d/regolamento_dei_corsi_di_formazione_per_volontari_e_dipendenti_della_croce_rossa_italiana.pdf'),
-            ('Schema per Equipollenze del 94-20', 'fa-book', 'https://datafiles.gaia.cri.it/media/filer_public/a6/1b/a61b14e5-7286-4028-8aeb-255e0a4a7209/schema_per_equipollenze_94-20_per_gaia.pdf'),
-            ('Schema per Equipollenze del 66-20', 'fa-book', 'https://datafiles.gaia.cri.it/media/filer_public/71/25/71253ac6-f878-46c7-88b9-68d93daa2e53/schema_per_equipollenze_del_66-20_per_gaia.pdf'),
+            ('Schema equipollenze Del №66/2020', 'fa-book', 'https://datafiles.gaia.cri.it/media/filer_public/71/25/71253ac6-f878-46c7-88b9-68d93daa2e53/schema_per_equipollenze_del_66-20_per_gaia.pdf'),
+            ('Schema equipollenze Del №94/2020', 'fa-book', 'https://datafiles.gaia.cri.it/media/filer_public/a6/1b/a61b14e5-7286-4028-8aeb-255e0a4a7209/schema_per_equipollenze_94-20_per_gaia.pdf'),
             ('Albo Informatizzato', 'fa-list', reverse(
                 'formazione:albo_info')) if to_show(me, RUBRICA_DELEGATI_OBIETTIVO_ALL + [GESTIONE_CORSI_SEDE]) else None,
         )),
