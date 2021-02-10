734d2e95f337d0c04dad1b6a32852b064651a583
vittorio.dargenio <vittorio.dargenio@citelgroup.it>
2021-02-10 17:51:56 +0100

    aggiunta appartenenza al modello

--- anagrafica/serializers.py
+++ anagrafica/serializers.py
@@ -1,14 +1,28 @@
+import hashlib
+
 from rest_framework import serializers
 
-from anagrafica.models import Persona
+from anagrafica.models import Persona, Appartenenza
+
+
+class AppartenenzaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Appartenenza
+        fields = ['inizio', 'fine', 'sede', 'membro']
 
 
 class PersonaSerializer(serializers.ModelSerializer):
-    my_field = serializers.SerializerMethodField('id_hash')
+    id = serializers.SerializerMethodField('get_id_hash')
+    id_persona = serializers.SerializerMethodField('get_id')
+    appartenenze = AppartenenzaSerializer(read_only=True, many=True)
 
-    def id_hash(self, instance):
-        return hash(instance.codice_fiscale)
+    def get_id(self, instance):
+        return instance.id
+
+    def get_id_hash(self, instance):
+        return hashlib.sha1(instance.codice_fiscale.encode('utf-8')).hexdigest()
 
     class Meta:
         model = Persona
-        fields = ['id', 'nome', 'cognome', 'codice_fiscale', 'my_field']
+        fields = ['id', 'id_persona', 'nome', 'cognome', 'codice_fiscale', 'appartenenze']
--- base/elasticsearch_function.py
+++ base/elasticsearch_function.py
@@ -1,7 +1,9 @@
+import json
+
 from anagrafica.models import Persona
 from anagrafica.serializers import PersonaSerializer
 
 
 def serializer_persona(idPersona):
     s_persona = PersonaSerializer(Persona.objects.get(pk=idPersona))
-    print(s_persona.data)
+    print(json.dumps(s_persona.data))
