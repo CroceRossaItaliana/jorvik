df9917decc619dba74356f023bb2d164144abbcd
vittorio.dargenio <vittorio.dargenio@citelgroup.it>
2021-02-11 11:33:51 +0100

    fix

--- anagrafica/management/commands/carica_volontari_elastic.py
+++ anagrafica/management/commands/carica_volontari_elastic.py
@@ -1,17 +1,33 @@
+import json
+
 from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
 
 from anagrafica.costanti import LOCALE, PROVINCIALE, REGIONALE, NAZIONALE
-from anagrafica.models import Sede
+from anagrafica.models import Sede, Persona
 from anagrafica.permessi.applicazioni import CONSIGLIERE_GIOVANE
+from anagrafica.serializers import PersonaSerializer
 from autenticazione.models import Utenza
 
 
 class Command(BaseCommand):
 
-    INDICE = 'indice'
+    help = 'Aggiungi curriculum della persona all\'indice inserito'
 
-    help = 'Aggiungi curriculum della persona all\'indice {}'.format(INDICE)
+    def add_arguments(self, parser):
+        parser.add_argument('index', nargs='?', type=str, default='')
 
     def handle(self, *args, **options):
-        print('handle')
+        index = options['index']
+        if index:
+            count = 0
+            for persona in Persona.objects.all():
+                s_persona = PersonaSerializer(persona)
+                data = json.dumps(s_persona.data)
+                print(data)
+                if count == 10:
+                    break
+                else:
+                    count += 1
+        else:
+            print('carica_volontari_elastic [index]')
