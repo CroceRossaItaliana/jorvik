5a54305de385775523d9a7a241c52f1e48e4387e
vittorio.dargenio <vittorio.dargenio@citelgroup.it>
2021-02-17 09:30:16 +0100

    fix titoli personagli signals.py, docker-compose-staging.yml

--- curriculum/signals.py
+++ curriculum/signals.py
@@ -1,12 +1,15 @@
+import json
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from kombu import uuid
 
+from anagrafica.serializers import PersonaSerializer
 from anagrafica.tasks import task_volontario_elastic
 from curriculum.models import TitoloPersonale
 
 
 @receiver(post_save, sender=TitoloPersonale)
 def save_titolo_personale(sender, instance, **kwargs):
-    task = task_volontario_elastic.apply_async(args=(instance.persona,), task_id=uuid())
-    return task
+    s_persona = PersonaSerializer(instance.persona)
+    task_volontario_elastic.apply_async(args=(json.dumps(s_persona.data),), task_id=uuid())
