57cba9ceabfddef4ff144056ac80171f32ea11e5
Luca Pisani <luca.pisani@abstract-technology.com>
2021-06-04 09:42:33 +0200

    GAIA-425: remove unused import

--- posta/viste.py
+++ posta/viste.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, get_object_or_404
 from django.utils.timezone import now
-from django.core.urlresolvers import reverse
 
 from anagrafica.models import Persona
 from anagrafica.permessi.costanti import ERRORE_PERMESSI
